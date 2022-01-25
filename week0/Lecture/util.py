25f7d0b5e88afa71f4c81bbb3a53ee32f0bcc05d
Mahdi Farrokh Baroughi <97599634+Mahdi-Farrokh-Baroughi@users.noreply.github.com>
2022-01-25 14:40:07 +0330

    Update util.py

--- week0/Lecture/util.py
+++ week0/Lecture/util.py
@@ -1,1 +1,55 @@
+class Node():
+    def __init__(self, state, parent, action, cost = None):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.cost = cost
 
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
+
+class Greedy_bestFirstSearch_frontier(StackFrontier):
+
+    def remove(self, maze_goal):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            min_Manhattan_distance = []
+            remove_node = []
+            for node in self.frontier:
+                i, j = node.state
+                m, n = maze_goal
+                Manhattan_distance = abs(m-i) + abs(n-j)
+                min_Manhattan_distance.append(Manhattan_distance)
+                remove_node.append(node)
+            self.frontier.remove(remove_node[min_Manhattan_distance.index(min(min_Manhattan_distance))])
+            return remove_node[min_Manhattan_distance.index(min(min_Manhattan_distance))]
